079c5e1f5e598018f225c1881c35ada33ff61982
Lisanne <lisanne.petracca@yahoo.com>
2023-11-03 22:00:16 -0500

    changed spacing

--- functions/removals_function.R
+++ functions/removals_function.R
@@ -1,6 +1,7 @@
 get.removals <- function(n.wolves.all.fxn, n.wolves.EWash.fxn, removal_rate){
   
   #in this function, we are removing wolves by pack in E Washington based on a removal rate
+  
   Nsamples <- dim(n.wolves.EWash.fxn)[1]
   
   for(i in 1:Nsamples){    
